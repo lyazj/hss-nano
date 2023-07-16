79538ec08acc52e186e748d7b58b3d009ab03b41
lyazj <seeson@pku.edu.cn>
2023-07-17 01:10:26 +0800

    Fix running scripts.

--- test/test_mc_UL18_MiniAODv2.sh
+++ test/test_mc_UL18_MiniAODv2.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 stdbuf -oL cmsDriver.py \
-    --python_filename test_mc_UL18_MiniAODv2.py \
-    -n 1000 \
     --mc \
+    -n 50 \
+    --nThreads 1 \
+    --python_filename RunIISummer20UL18NanoAODv9.py \
     --eventcontent NANOAODSIM \
     --datatier NANOAODSIM \
-    --conditions 106X_upgrade2018_realistic_v15_L1v1 \
+    --conditions 106X_upgrade2018_realistic_v16_L1v1 \
     --step NANO \
-    --nThreads 1 \
     --era Run2_2018,run2_nanoAOD_106Xv2 \
     --customise PhysicsTools/NanoTuples/nanoTuples_cff.nanoTuples_customizeMC \
     --filein /store/mc/RunIISummer20UL18MiniAODv2/WplusH_HToCC_WToLNu_M-125_TuneCP5_13TeV-powheg-pythia8/MINIAODSIM/106X_upgrade2018_realistic_v16_L1v1-v1/2520000/A4FFC5A7-79DF-FE4B-A515-1F3EA0513509.root \
-    --fileout file:HIG-RunIISummer20UL18CustomizedNanoAOD-mc-A4FFC5A7-79DF-FE4B-A515-1F3EA0513509.root \
+    --fileout file:RunIISummer20UL18NanoAODv9-A4FFC5A7-79DF-FE4B-A515-1F3EA0513509.root \
     2>&1 | tee test_mc_UL18_MiniAODv2.log
