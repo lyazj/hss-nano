d13e4166504adf71ab58ae18397d1c1693557396
lyazj <seeson@pku.edu.cn>
2023-07-17 01:10:26 +0800

    Fix running scripts.

--- test/test_MiniAODv2_UL18.sh
+++ test/test_MiniAODv2_UL18.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 stdbuf -oL cmsDriver.py \
-    --python_filename test_MiniAODv2_UL18.py \
-    -n 1000 \
     --mc \
+    -n 1000 \
+    --nThreads 1 \
+    --python_filename test_MiniAODv2_UL18.py \
     --eventcontent NANOAODSIM \
     --datatier NANOAODSIM \
-    --conditions 106X_upgrade2018_realistic_v15_L1v1 \
+    --conditions 106X_upgrade2018_realistic_v16_L1v1 \
     --step NANO \
-    --nThreads 1 \
     --era Run2_2018,run2_nanoAOD_106Xv2 \
     --customise PhysicsTools/NanoTuples/nanoTuples_cff.nanoTuples_customizeMC \
     --filein file:HIG-RunIISummer20UL18MiniAODv2-02334_15946930_1001_1.root \
